3c0c2e597c913b7ec1402f2f15880b753067ebe7
Evgenii <evtatdevelop@gmail.com>
2023-08-20 19:59:29 +0700

    fixed some bugs

--- src/results.js
+++ src/results.js
@@ -4,34 +4,29 @@ const { getCardSum, bust, showCards } = require("./cardDeck")
 const showResults = (winer, casino, player) => {
   console.log('\n');
   switch ( winer ) {
-    case 'casino': 
-      console.log(`Casino winner`);
+    case 'casino':  console.log(`Casino winner`);
       stat('casino'); break;
-    case 'player':
-      console.log(`You are a winner`);
+    case 'player': console.log(`You are a winner`);
       stat('player'); break;
-    case 'bust player':
-      console.log(`\nYou have too much. Casino winner`);
-      stat('player'); break;
-    case 'bust casino':
-      console.log(`\nOverkill at the casino. You are a winner`);
+    case 'bust player': console.log(`\nYou have too much. Casino winner`);
+      stat('casino'); break;
+    case 'bust casino': console.log(`\nOverkill at the casino. You are a winner`);
       stat('player'); break;
     case 'draw': console.log(`Draw`);                
   } 
-
+  showEwsult = true;
   console.log(`You: ${getCardSum(player)}:`);
   showCards(player)
   console.log(`Casino: ${getCardSum(casino)}:`);
   showCards(casino)
 }
 
+let showEwsult = false;
 
 const getWinner = (action, casino, player) => {
   let winer = null;
   let bst = false;
-  const casinoRate = getCardSum(casino);
-  const playerRate = getCardSum(player);
-  
+
   if ( bust(player) ) { 
     winer = 'bust player';
     bst = true;
@@ -41,11 +36,13 @@ const getWinner = (action, casino, player) => {
     bst = true;
   }
   if ( !bst && action === 'stop' ) {
-    if ( casinoRate > playerRate ) winer = 'casimo'
-    if ( casinoRate < playerRate ) winer = 'player'  
-    if ( casinoRate === playerRate ) winer = 'draw'    
+    const casinoRate = getCardSum(casino);
+    const playerRate = getCardSum(player);
+    if ( casinoRate > playerRate ) winer = 'casino'
+    else if ( casinoRate < playerRate ) winer = 'player'  
+    else winer = 'draw'    
   }
-  if ( winer ) showResults(winer, casino, player);
+  if ( winer && !showEwsult ) showResults(winer, casino, player);
   return winer;
 }
 
